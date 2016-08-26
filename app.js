a8123c05254ad0a5a3d667913eb6c3fbdc7cd143
Corey Peck <coreypeck@gmail.com>
2016-08-26 11:30:41 -0500

    Delete is now working, but Monthly totals aren't updating correctly

--- app.js
+++ app.js
@@ -4,46 +4,60 @@ myApp.controller("empController", ["$scope", function($scope) {
     console.log("I'm working!");
     var i = 0;
     setBlank($scope);
-    $scope.summary = [{}];
-    var currentSummary = [{}];
+    $scope.summary = [];
+    $scope.salaryTotal = 0;
     $scope.getEmp = function() {
         sendEmp($scope, i);
         i++;
     }
-    $scope.delEmp = function(id, empSal) {
-      i--;
-      var counter=0;
-      $scope.summary.forEach(function(worker) {
-        $scope.summary = [{}];
-        currentSummary = [{}];
-        console.log("id",id);
-        console.log("worker",worker.arrayNum);
-        if(worker.arrayNum == id){
-          return;
-        }
-        else{
-          currentSummary.push(worker);
-          currentSummary[counter].arrayNum = counter;
-          currentSummary.shift();
-          $scope.summary = currentSummary;
-          console.log(currentSummary);
-          console.log($scope.summary);
-          prepPush($scope, currentSummary[counter].first,currentSummary[counter].last,currentSummary[counter].num,currentSummary[counter].title,currentSummary[counter].sal,currentSummary[counter].monTotal,currentSummary[counter].arrayNum);
-          counter++;
-        }
-      });
+    $scope.delEmp = function(id) {
+        console.log(id);
+        $scope.salaryTotal -= $scope.summary[id].newEmp.monSal;
+        $scope.summary.splice(id, 1);
+        i--;
+        //   empHolder = [];
+        //   i--;
+        //   var counter=0;
+        //   $scope.summary.forEach(function(worker) {
+        //     console.log("id",id);
+        //     console.log("worker",worker.newEmp.arrayNum);
+        //
+        //     if(worker.newEmp.arrayNum == id){
+        //       if($scope.summary.length == 1){
+        //         var myEl = angular.element( document.querySelector( '#container' ) );
+        //         $scope.summary = [];
+        //         myEl.empty();
+        //       }
+        //       return;
+        //     }
+        //     else{
+        //       var currentSummary = [];
+        //       console.log("Worker",worker);
+        //       console.log("Current before math", currentSummary);
+        //       worker.newEmp.arrayNum = counter;
+        //       console.log("Worker",worker);
+        //       worker.newEmp.monTotal = (worker.newEmp.monTotal - worker.newEmp.monSal);
+        //       console.log("Worker",worker);
+        //       currentSummary.push(worker);
+        //       empHolder.push(worker);
+        //       // currentSummary[counter].empNum.arrayNum = counter;
+        //       console.log("Current after math", currentSummary);
+        //       // currentSummary.shift();
+        //       prepPush($scope, currentSummary);
+        //       counter++;
+        //     }
+        //   });
+        // }
     }
 }]);
-
-var monthlySalary = 0;
 
 function sendEmp($scope, i) {
     if ($scope.empFName == "" || $scope.empLName == "" || $scope.empNum == 0 || $scope.empTitle == "") {
         console.log("Make sure you have all fields filled out and all number values aren't 0");
+        return;
     } else {
-        if (i == 0) {
-            $scope.summary.shift();
-        }
+        // var myEl = angular.element( document.querySelector( '#container' ) );
+        // myEl.empty();
         pushArray($scope, i);
     }
 }
@@ -57,32 +71,34 @@ function setBlank($scope, i) {
 }
 
 function pushArray($scope, i) {
-    var first = $scope.empFName;
-    var last = $scope.empLName;
-    var num = $scope.empNum;
-    var title = $scope.empTitle;
-    var sal = $scope.empSal;
-    var monTotal = 0;
-    var arrayNum = i;
+    var newEmp = new Employee($scope, i);
+    console.log(newEmp)
     if (i == 0) {
-        prepPush($scope, first, last, num, title, sal, monTotal, arrayNum);
+        prepPush($scope, newEmp);
     } else {
-        prepPush($scope, first, last, num, title, sal, monTotal, arrayNum);
-        $scope.summary[0].monTotal += $scope.summary[arrayNum].monTotal;
+        prepPush($scope, newEmp);
+        console.log("Scope before monTotal added", $scope.summary);
+        $scope.salaryTotal += $scope.summary[i].newEmp.monTotal;
     }
 }
 
-function prepPush($scope, first, last, num, title, sal, monTotal, arrayNum) {
-    monTotal = (sal / 12);
-    console.log(monTotal);
+function Employee($scope, i) {
+    this.first = $scope.empFName;
+    this.last = $scope.empLName;
+    this.num = $scope.empNum;
+    this.title = $scope.empTitle;
+    this.sal = $scope.empSal;
+    this.monSal = ($scope.empSal / 12);
+    this.monTotal = 0;
+}
+
+function prepPush($scope, newEmp) {
+
+    newEmp.monTotal = (newEmp.monSal);
+    console.log(newEmp.monTotal);
     $scope.summary.push({
-        first: first,
-        last: last,
-        num: num,
-        title: title,
-        sal: sal,
-        monTotal: monTotal,
-        arrayNum: arrayNum
+        newEmp
     });
+    console.log("scope.summary", $scope.summary);
     setBlank($scope);
 }
